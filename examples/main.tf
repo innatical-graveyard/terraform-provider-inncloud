2bc6cb5a3e40b863fe054f840d379d08648353db
Lleyton Gray <lleyton@innatical.com>
2021-08-01 14:52:13 -0700

    update example

--- examples/main.tf
+++ examples/main.tf
@@ -7,31 +7,23 @@ terraform {
   }
 }
 
+variable "token" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
 provider "inncloud" {
-  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Mjc2ODA2MDgsImF1ZCI6InNreWUuaW5uYXRpY2FsLmNvbSIsInN1YiI6IjQ4YmQ1M2IwLTYyZTQtNGJmYy1iNWQ5LTQxZGVjMTg1ODhkNiJ9.IMRYSzRSNZAd-1op6v_Vz505YNN656qjoLadSa4jeoQ"
-  project_id = "57372f3d-25c3-4041-a2a2-bfdc353b90c9" 
+  token = var.token
+  project_id = var.project_id
 }
 
-resource "inncloud_server" "UwU" {
-  name = "UwU"
+resource "inncloud_server" "Example" {
+  name = "Example"
   model = "starter"
   image = "ubuntu-20.04"
-  region = "HEL1"
-  cycle = "month"
-}
-
-resource "inncloud_server" "fuck" {
-  name = "fuck"
-  model = "pro"
-  image = "ubuntu-20.04"
-  region = "HEL1"
-  cycle = "month"
-}
-
-resource "inncloud_server" "yes" {
-  name = "yes"
-  model = "starter"
-  image = "ubuntu-20.04"
-  region = "HEL1"
+  region = "LA1"
   cycle = "month"
 }
